ff5c4c6a8b43c8152514235642bbc7882c4f77a5
opmmr <163329424+opmmr@users.noreply.github.com>
2024-06-22 12:13:02 +0200

    implemented function

--- main.rs
+++ main.rs
@@ -1,20 +1,49 @@
-use actix_web::{web, App, HttpServer, Responder, Result};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 use std::env;
+use serde_json::json;
+use actix_web::{middleware, Error};
+use serde::{Deserialize, Serialize};
 
-async fn exchange_tokens() -> impl Responder {
-    format!("Token exchange endpoint")
+#[derive(Serialize, Deserialize)]
+struct ExchangeStats {
+    num_requests: u64,
 }
 
-async fn user_interaction() -> impl Responder {
-    format!("User interaction endpoint")
+static mut EXCHANGE_STATS: ExchangeStats = ExchangeStats { num_requests: 0 };
+
+async fn exchange_tokens() -> Result<impl Responder, Error> {
+    unsafe {
+        EXCHANGE_STATS.num_requests += 1;
+    }
+
+    Ok(HttpResponse::Ok().json(json!({
+        "message": "Token exchange endpoint",
+        "stats": EXCHANGE_STATS,
+    })))
+}
+
+async fn user_interaction() -> Result<impl Responder, Error> {
+    Ok(format!("User interaction endpoint"))
+}
+
+async fn stats() -> Result<impl Responder, Error> {
+    let stats;
+    unsafe {
+        stats = json!({
+            "num_requests": EXCHANGE_TS.num_requests,
+        });
+    }
+    
+    Ok(HttpResponse::Ok().json(stats))
 }
 
 fn config_services(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api")
             .route("/exchange_tokens", web::get().to(exchange_tokens))
-            .route("/user_interaction", web::get().to(user_interaction)),
+            .route("/user_interaction", web::get().to(user_interaction))
+            .route("/stats", web::get().to(stats)),
     );
 }
 
@@ -24,7 +53,9 @@ async fn main() -> std::io::Result<()> {
     let server_url = env::var("SERVER_URL").unwrap_or_else(|_| "127.0.0.1:8080".to_string());
 
     HttpServer::new(|| {
-        App::new().configure(config_services) // Modularizing route configuration
+        App::new()
+            .wrap(middleware::Logger::default())
+            .configure(config_services)
     })
     .bind(&server_url)?
     .run()
