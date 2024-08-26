1fcebd9175d97b0d26a232056658441c146395b5
opmmr <163329424+opmmr@users.noreply.github.com>
2024-08-26 07:24:19 +0200

    error catching update

--- blockchain/token_contract.rs
+++ blockchain/token_contract.rs
@@ -1,5 +1,6 @@
 use ethcontract::prelude::*;
 use std::{env, fs, path::Path};
+use anyhow::{Result, Context};
 
 #[derive(Clone)]
 pub struct RustTokenExchange {
@@ -8,44 +9,44 @@ pub struct RustTokenExchange {
 }
 
 impl RustTokenExchange {
-    pub async fn new() -> Self {
+    pub async fn new() -> Result<Self> {
         dotenv::dotenv().ok();
-        let http = Http::new(&env::var("NODE_URL").expect("NODE_URL must be set"))
-            .expect("Error creating HTTP transport");
+        let http = Http::new(&env::var("NODE_URL").context("NODE_URL must be set")?)
+            .context("Error creating HTTP transport")?;
         let web3 = Web3::new(http);
-        let contract_abi_path = Path::new(&env::var("CONTRACT_ABI_PATH").expect("CONTRACT_ABI_PATH must be set"));
+        let contract_abi_path = Path::new(&env::var("CONTRACT_ABI_PATH").context("CONTRACT_ABI_PATH must be set")?);
         let contract_abi = fs::read_to_string(contract_abi_path)
-            .expect("Could not load contract ABI");
+            .context("Could not load contract ABI")?;
         let contract_address = env::var("CONTRACT_ADDRESS")
-            .expect("CONTRACT_ADDRESS must be set")
+            .context("CONTRACT_ADDRESS must be set")?
             .parse::<Address>()
-            .expect("Failed to parse CONTRACT_ADDRESS");
+            .context("Failed to parse CONTRACT_ADDRESS")?;
         let contract = Contract::from_json(
             web3.eth(),
             contract_address,
             contract_abi.as_bytes(),
         )
-        .expect("Error creating contract instance");
+        .context("Error creating contract instance")?;
 
-        RustTokenExchange { web3, contract }
+        Ok(RustTokenExchange { web3, contract })
     }
 
-    pub async fn deploy(&self) -> Result<TxHash, Box<dyn std::error::Error>> {
+    pub async fn deploy(&self) -> Result<TxHash> {
         unimplemented!()
     }
 
-    pub async fn transfer_tokens(&self, to: Address, value: U256) -> Result<TxHash, Box<dyn std::error::Error>> {
+    pub async fn transfer_tokens(&self, to: Address, value: U256) -> Result<TxHash> {
         self.contract
             .method::<_, H256>("transfer", (to, value))
-            .expect("transfer function not found")
+            .context("transfer function not found")?
             .send()
             .await
             .map_err(Into::into)
     }
-
 }
 
 #[tokio::main]
-async fn main() {
-    let exchange = RustTokenExchange::new().await;
+async fn main() -> Result<()> {
+    let exchange = RustTokenExchange::new().await.context("Failed to create RustTokenExchange")?;
+    Ok(())
 }
